e94f19f34403e08dc1433375bc9a2d3262bbdcce
Alissa <alissayarmantho1@gmail.com>
2021-05-31 01:20:24 +0800

    Add Results Page

--- src/views/pages/result/Result.tsx
+++ src/views/pages/result/Result.tsx
@@ -1,35 +1,69 @@
-import Button from "react-bootstrap/Button";
+import React, { useState } from "react";
 import FoodPlaceCard from "../../components/FoodPlaceCard/FoodPlaceCard";
 import MapContainer from "../../components/MapContainer/MapContainer";
-import classnames from "classnames";
-import { X } from "react-feather";
+import SearchBox from "../../components/SearchBox/SearchBox";
 import "./Result.scss";
 
 const Result: React.FC = () => {
+  const [searchPlace, setSearchPlace] = useState<string>("");
   const handleClick = () => console.log("Hello World");
   var date = new Date();
   return (
-    <div>
-      <MapContainer />
-      <FoodPlaceCard
-        imageArray={[
-          "https://i.ibb.co/Jct7bBZ/kfc.jpg",
-          "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
-        ]}
-        title="KFC"
-        location="Buona Vista"
-        rating={5}
-        foodTypes={["Fast Food", "Fried Chicken"]}
-        servingTime={[date, date]}
-      ></FoodPlaceCard>
-      <Button
-        type="button"
-        className={classnames("btn btn-outline-secondary btn-svg", "delete")}
-        aria-label="button"
-        onClick={handleClick}
-      >
-        <X className="X" />
-      </Button>
+    <div className="mainContainer">
+      <div className="mapWrapper">
+        <MapContainer />
+      </div>
+      <div className="resultListWrapper">
+        <div className="searchPlace">
+          <SearchBox
+            value={searchPlace}
+            placeholder="Type your location here"
+            onChange={(e) => setSearchPlace(e.target.value)}
+          />
+        </div>
+        <FoodPlaceCard
+          imageArray={[
+            "https://i.ibb.co/Jct7bBZ/kfc.jpg",
+            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+          ]}
+          title="KFC"
+          location="Buona Vista"
+          rating={5}
+          foodTypes={["Fast Food", "Fried Chicken"]}
+          servingTime={[date, date]}
+        ></FoodPlaceCard>
+        <FoodPlaceCard
+          imageArray={[
+            "https://i.ibb.co/Jct7bBZ/kfc.jpg",
+            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+          ]}
+          title="KFC"
+          location="Buona Vista"
+          rating={5}
+          foodTypes={["Fast Food", "Fried Chicken"]}
+          servingTime={[date, date]}
+        ></FoodPlaceCard>
+        <FoodPlaceCard
+          imageArray={[
+            "https://i.ibb.co/Jct7bBZ/kfc.jpg",
+            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+          ]}
+          title="KFC"
+          location="Buona Vista"
+          rating={5}
+          foodTypes={["Fast Food", "Fried Chicken"]}
+          servingTime={[date, date]}
+        ></FoodPlaceCard>
+      </div>
     </div>
   );
 };
