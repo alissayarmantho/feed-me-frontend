a5d3ef287f2db3b600e3216db1fd22029793c61c
Alissa <alissayarmantho1@gmail.com>
2021-05-31 03:01:26 +0800

    Add line break and fillerValue array

--- src/views/pages/result/Result.tsx
+++ src/views/pages/result/Result.tsx
@@ -4,12 +4,86 @@ import MapContainer from "../../components/MapContainer/MapContainer";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import "./Result.scss";
 
+var date = new Date();
+const fillerValues = [
+  {
+    imageArray: [
+      "https://i.ibb.co/Jct7bBZ/kfc.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+    ],
+    title: "KFC",
+    location: "Buona Vista",
+    rating: 5,
+    foodTypes: ["Fast Food", "Fried Chicken"],
+    servingTime: [date, date],
+  },
+  {
+    imageArray: [
+      "https://i.ibb.co/Jct7bBZ/kfc.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+    ],
+    title: "KFC",
+    location: "Buona Vista",
+    rating: 5,
+    foodTypes: ["Fast Food", "Fried Chicken"],
+    servingTime: [date, date],
+  },
+  {
+    imageArray: [
+      "https://i.ibb.co/Jct7bBZ/kfc.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+    ],
+    title: "KFC",
+    location: "Buona Vista",
+    rating: 5,
+    foodTypes: ["Fast Food", "Fried Chicken"],
+    servingTime: [date, date],
+  },
+  {
+    imageArray: [
+      "https://i.ibb.co/Jct7bBZ/kfc.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+    ],
+    title: "KFC",
+    location: "Buona Vista",
+    rating: 5,
+    foodTypes: ["Fast Food", "Fried Chicken"],
+    servingTime: [date, date],
+  },
+  {
+    imageArray: [
+      "https://i.ibb.co/Jct7bBZ/kfc.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+      "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
+    ],
+    title: "KFC",
+    location: "Buona Vista",
+    rating: 5,
+    foodTypes: ["Fast Food", "Fried Chicken"],
+    servingTime: [date, date],
+  },
+];
+
 const Result: React.FC = () => {
   const [searchPlace, setSearchPlace] = useState<string>("");
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handleClick = () => console.log("Hello World");
-  var date = new Date();
+
   return (
     <div className="mainContainer">
       <div className="mapWrapper">
@@ -23,48 +97,20 @@ const Result: React.FC = () => {
             onChange={(e) => setSearchPlace(e.target.value)}
           />
         </div>
-        <FoodPlaceCard
-          imageArray={[
-            "https://i.ibb.co/Jct7bBZ/kfc.jpg",
-            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
-            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
-            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
-            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
-          ]}
-          title="KFC"
-          location="Buona Vista"
-          rating={5}
-          foodTypes={["Fast Food", "Fried Chicken"]}
-          servingTime={[date, date]}
-        ></FoodPlaceCard>
-        <FoodPlaceCard
-          imageArray={[
-            "https://i.ibb.co/Jct7bBZ/kfc.jpg",
-            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
-            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
-            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
-            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
-          ]}
-          title="KFC"
-          location="Buona Vista"
-          rating={5}
-          foodTypes={["Fast Food", "Fried Chicken"]}
-          servingTime={[date, date]}
-        ></FoodPlaceCard>
-        <FoodPlaceCard
-          imageArray={[
-            "https://i.ibb.co/Jct7bBZ/kfc.jpg",
-            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
-            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
-            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
-            "https://i.ibb.co/0Y3BNm9/kfc2.jpg",
-          ]}
-          title="KFC"
-          location="Buona Vista"
-          rating={5}
-          foodTypes={["Fast Food", "Fried Chicken"]}
-          servingTime={[date, date]}
-        ></FoodPlaceCard>
+        {fillerValues.map((value, index) => (
+          <div key={index}>
+            <FoodPlaceCard
+              imageArray={value.imageArray}
+              title={value.title}
+              location={value.location}
+              rating={value.rating}
+              foodTypes={value.foodTypes}
+              servingTime={value.servingTime}
+              key={index}
+            ></FoodPlaceCard>
+            {index !== fillerValues.length - 1 && <hr className="lineBreak" />}
+          </div>
+        ))}
       </div>
     </div>
   );
